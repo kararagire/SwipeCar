1cae22053f931269002c550a572421ce08aba7ac
Acro <kararagire0@outlook.jp>
2021-06-17 23:13:33 +0900

    4章効果音まだ

---
+++ Assets/CarController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 0;
+    public bool notsecond = true;
+    Vector2 startPos;
+    float startTime;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && notsecond)
+
+        {
+            this.startPos = Input.mousePosition;
+            startTime = Time.time;
+        }
+        else if (Input.GetMouseButtonUp(0) && notsecond)
+        {
+            Vector2 endPos = Input.mousePosition;
+            float endTime = Time.time;
+            float swipeLength = endPos.x - this.startPos.x;
+            float swipeTime = endTime - startTime;
+
+            this.speed = swipeLength / (swipeTime * 8500.0f);
+            notsecond = false;
+        }
+        transform.Translate(this.speed, 0, 0);
+        this.speed *= 0.98f;
+
+    }
+}
---
+++ Assets/GameDirector.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject car;
+    GameObject flag;
+    GameObject distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.car = GameObject.Find("car");
+        this.flag = GameObject.Find("flag");
+        this.distance = GameObject.Find("Distance");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = car.GetComponent<CarController>().speed;
+        bool notsecond = car.GetComponent<CarController>().notsecond;
+        float length = this.flag.transform.position.x - this.car.transform.position.x;
+        if (speed < 0.1f && !notsecond || length <= -0.1)
+        {
+            this.distance.GetComponent<Text>().text = "ゴールまで" + length.ToString("F2") + "m";
+        } else if (notsecond)
+        {
+            this.distance.GetComponent<Text>().text = "ゴールまで" + length.ToString("F2") + "m";
+        } else
+        {
+            this.distance.GetComponent<Text>().text = "";
+        }
+
+
+    }
+}
